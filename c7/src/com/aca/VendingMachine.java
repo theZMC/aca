d08d6ac3f0af64a567796e7afb2e5ecc1c32f3b4
Zach Callahan <zach@zmc.dev>
2021-03-16 19:15:37 -0500

    Added comments

--- c7/src/com/aca/VendingMachine.java
+++ c7/src/com/aca/VendingMachine.java
@@ -4,33 +4,63 @@ import java.util.Scanner;
 
 public class VendingMachine {
     public static void main(String[] args) {
-        int[] sales = new int[5];
+        vend();
+    }
+    
+    private static void vend(){
+        // Use an array to keep track of how many of each item has been purchased
+        int[]   sales   = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        int     pick;
 
+        // the program should keep accepting new requests for product forever
         while(true){
-            Scanner scanner = new Scanner(System.in);
-            int pick = getCustomerPick(scanner);
 
+            // allows a customer to select from 5 delicious instantly prepared meals
+            pick = getCustomerPick(scanner);
+
+            // the secret number 99 causes...
             if(pick == 99){
+                // the program to say "Goodbye!"...
                 System.out.println("\nGoodbye!");
+
+                // printing a final list of how many of each item was sold...
                 printSales(sales);
+                
+                // and quit running.
                 scanner.close();
                 return;
             }
 
-            sales[pick] = sales[pick] + 1;
+            // track how many of each item is sold
+            sales[pick]++;
+
+            // After each purchase
+            // output the number of each item sold so far
             printSales(sales);
         }
     }
 
+    // allows the customer to pick item 0 to 4
     private static int getCustomerPick(Scanner scanner) {
-        System.out.println("\nPick an item from 0 to 4.");
-        
-        int pick = scanner.nextInt();
-        scanner.nextLine();
-        
+        boolean waiting = true;
+        int pick = 0;
+        while(waiting){
+            System.out.println("\nPick an item from 0 to 4.");
+
+            if(scanner.hasNextInt()){
+                pick = scanner.nextInt();
+                if(pick == 99 || (pick <= 4 && pick >= 0)){
+                    waiting = false;
+                }
+            }
+
+            scanner.nextLine();
+        }
         return pick;
     }
 
+    // output the number of each item sold so far
     private static void printSales(int[] sales) {
         System.out.println("\nLifetime sales summary:");
         for(int i = 0; i < sales.length; i++){

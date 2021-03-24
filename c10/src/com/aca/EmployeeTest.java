162263b9f3bc8a95c2af0e8fbe824d157894505f
Zach Callahan <zach@zmc.dev>
2021-03-24 11:42:51 -0500

    more formatting changes

--- c10/src/com/aca/EmployeeTest.java
+++ c10/src/com/aca/EmployeeTest.java
@@ -7,19 +7,24 @@ public class EmployeeTest {
     public static void main(String[] args) {
         Employee[] employees = {
             //          firstName   lastName    birthday                    hireDate
-            new Employee("dan",     "Lazenby",  LocalDate.of(1967,3,23),    LocalDate.now()),
+            new Employee("dan",     "Lazenby",  LocalDate.of(1967,3,24),    LocalDate.now()),
             new Employee("zach",    "callahan", LocalDate.of(1990,9,3),     LocalDate.of(1981,3,23)),
             new Employee("alison",  "wish",     LocalDate.of(1969,3,23),    LocalDate.now())        
         };
-        
-        printEmployees(employees);
-        
-        System.out.println("\nBirthdays:");
-        printEmployees(getEmployeesBornToday(employees));
 
         int tenureThreshold = 40;
-        System.out.println("\nTenured Employees (" + tenureThreshold + "+ years):");
-        printEmployees(getTenuredEmployees(employees, tenureThreshold));
+        
+        Employee[] employeesBornToday   = getEmployeesBornToday(employees);
+        Employee[] tenuredEmployees     = getTenuredEmployees(employees, tenureThreshold);
+
+        System.out.printf("\nAll Employees:\n");
+        printEmployees(employees);
+        
+        System.out.printf("\nToday's Birthdays:\n");
+        printEmployees(employeesBornToday);
+
+        System.out.printf("\nTenured Employees (%s+ years):\n", tenureThreshold);
+        printEmployees(tenuredEmployees);
     }
 
     private static Employee[] getTenuredEmployees(Employee[] employees, int tenureThreshold){
@@ -35,8 +40,9 @@ public class EmployeeTest {
     }
 
     private static void printEmployees(Employee[] employees){
+        System.out.printf(Employee.getOutputFormat() + "\n", "Full Name","Birthday","Hire Date");
         for(Employee employee : employees){
-            System.out.println(employee.getFullName());
+            System.out.println(employee);
         }
     }
 }

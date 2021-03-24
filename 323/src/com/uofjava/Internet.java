74d3b62d285b79f895aff6aad8659c7dd863fb0d
Zach Callahan <zach@zmc.dev>
2021-03-24 12:11:30 -0500

    Finished the internet code from hw

--- 323/src/com/uofjava/Internet.java
+++ 323/src/com/uofjava/Internet.java
@@ -3,11 +3,15 @@ package com.uofjava;
 import java.time.LocalDate;
 
 public class Internet {
+    private static String stringFormat = "%-10s %12s %12s %12s %12s";
+    private static String speedFormat  = "%4dMb/s";
+
     private String      productId;
     private int         uploadSpeed;
     private int         downloadSpeed;
     private LocalDate   startDate;
     private LocalDate   endDate;
+    
 
     public Internet(String productId, int uploadSpeed, int downloadSpeed, LocalDate startDate, LocalDate endDate){
         this.productId      = productId;
@@ -17,23 +21,35 @@ public class Internet {
         this.endDate        = endDate;
     }
 
+    public String toString() {
+        String uploadSpeedString = String.format(speedFormat, uploadSpeed);
+        String downloadSpeedString = String.format(speedFormat, downloadSpeed);
+        return String.format(stringFormat, productId, uploadSpeedString, downloadSpeedString, startDate, endDate);
+    }
+    public String toString(String customFormat) {
+        String uploadSpeedString = String.format(speedFormat, uploadSpeed);
+        String downloadSpeedString = String.format(speedFormat, downloadSpeed);
+        return String.format(customFormat, productId, uploadSpeedString, downloadSpeedString, startDate, endDate);
+    }
     public String getProductId() {
         return productId;
     }
-
     public int getUploadSpeed() {
         return uploadSpeed;
     }
-
     public int getDownloadSpeed() {
         return downloadSpeed;
     }
-
     public LocalDate getStartDate() {
         return startDate;
     }
-
     public LocalDate getEndDate() {
         return endDate;
+    }
+    public static String getStringFormat() {
+        return stringFormat;
+    }
+    public static String getSpeedFormat() {
+        return speedFormat;
     }
 }
---
+++ 323/src/com/uofjava/InternetTest.java
@@ -0,0 +1,26 @@
+package com.uofjava;
+
+import java.time.LocalDate;
+
+public class InternetTest {
+    public static void main(String[] args) {
+        Internet[] internets = {
+            new Internet("Premier", 20, 200, LocalDate.of(2019,6,22), LocalDate.of(2022,6,22)),
+            new Internet("Ultimate", 50, 600, LocalDate.of(2019,6,22), LocalDate.of(2022,6,22)),
+            new Internet("Basic", 10, 50, LocalDate.of(2019,6,22), LocalDate.of(2022,6,22)),
+            new Internet("Barely", 1, 5, LocalDate.of(2019,6,22), LocalDate.of(2022,6,22))
+        };
+
+        printInternets(internets);
+    }
+
+    private static void printInternets(Internet[] internets) {
+        String header = String.format(Internet.getStringFormat(), "Product ID", "Speed Up", "Speed Down", "Start Date", "End Date");
+        
+        System.out.println();
+        System.out.println((char)27 + "[4m" + header + (char)27 + "[0m");
+        for(Internet internet : internets) {
+            System.out.println(internet);
+        }       
+    }
+}

8eb48e4590c79f60aca940c418b7eeb701a215b1
Zach Callahan <zach@zmc.dev>
2021-03-24 11:12:07 -0500

    Greatly simplified the birthday and tenure logic

--- c10/src/com/aca/Employee.java
+++ c10/src/com/aca/Employee.java
@@ -3,7 +3,7 @@ package com.aca;
 import java.time.LocalDate;
 import java.time.Period;
 
-public class Employee extends Object{
+public class Employee{
     private String      firstName;
     private String      lastName;
     private LocalDate   birthday;
@@ -17,16 +17,15 @@ public class Employee extends Object{
     }
 
     public String getFullName() {
-        String firstName = "";
-        String lastName = "";
-        if(this.firstName.length() > 0){
-            firstName = this.firstName.substring(0,1).toUpperCase() + this.firstName.substring(1);
-        }
-
-        if(this.lastName.length() > 0){
-            lastName = this.lastName.substring(0,1).toUpperCase() + this.lastName.substring(1);
-        }
-        return firstName + " " + lastName;
+        String first    = firstName.toLowerCase();
+        String last     = lastName.toLowerCase();
+        first = first.length() > 0 ? 
+            first.substring(0,1).toUpperCase() + first.substring(1) : 
+            "";
+        last = last.length() > 0 ? 
+            last.substring(0,1).toUpperCase() + last.substring(1) : 
+            "";
+        return first + " " + last;
     }
 
     public boolean hasTenured(int tenureThreshold){
@@ -39,11 +38,15 @@ public class Employee extends Object{
         return interval.getDays() == 0 && interval.getMonths() == 0;
     }
 
-    @Override
     public String toString(){
-        String format = "Name: %s\tBirthday: %s\tHire Date: %s";
+        String format = "%s %s %s";
         return String.format(format, getFullName(), getBirthday(), getHireDate());
     }
+
+    public String toString(String format){
+        return String.format(format, getFullName(), getBirthday(), getHireDate());
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -53,7 +56,6 @@ public class Employee extends Object{
     public LocalDate getBirthday() {
         return birthday;
     }
-
     public LocalDate getHireDate() {
         return hireDate;
     }
--- c10/src/com/aca/EmployeeTest.java
+++ c10/src/com/aca/EmployeeTest.java
@@ -1,13 +1,15 @@
 package com.aca;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class EmployeeTest {
     public static void main(String[] args) {
         Employee[] employees = {
-            new Employee("dan", "Lazenby", LocalDate.of(1967,3,23), LocalDate.now()),
-            new Employee("Zach", "callahan", LocalDate.of(1990,9,3), LocalDate.of(1981,3,23)),
-            new Employee("alison", "wish", LocalDate.of(1969,3,23), LocalDate.now())        
+            //          firstName   lastName    birthday                    hireDate
+            new Employee("dan",     "Lazenby",  LocalDate.of(1967,3,23),    LocalDate.now()),
+            new Employee("zach",    "callahan", LocalDate.of(1990,9,3),     LocalDate.of(1981,3,23)),
+            new Employee("alison",  "wish",     LocalDate.of(1969,3,23),    LocalDate.now())        
         };
         
         printEmployees(employees);
@@ -21,65 +23,20 @@ public class EmployeeTest {
     }
 
     private static Employee[] getTenuredEmployees(Employee[] employees, int tenureThreshold){
-        int[]   tenuredIndexes  = new int[employees.length];
-        int     numOfTenured    = 0;
-
-        for(int i = 0; i < employees.length; i++){
-            if(employees[i].hasTenured(tenureThreshold)){
-                tenuredIndexes[i] = i;
-                numOfTenured++;
-            } else {
-                tenuredIndexes[i] = -1;
-            }
-        }
-
-        Employee[] out = new Employee[numOfTenured];
-        
-        if(numOfTenured > 0){
-            int outIndex = 0;
-
-            for(int i = 0; i < employees.length; i++){
-                if(tenuredIndexes[i] == i){
-                    out[outIndex] = employees[i];
-                    outIndex++;
-                }
-            }
-        }
-
-        return out;
+        return Arrays.stream(employees)
+            .filter(employee -> employee.hasTenured(tenureThreshold))
+            .toArray(Employee[]::new);
     }
-    
+
     private static Employee[] getEmployeesBornToday(Employee[] employees){
-        int[]   bDayIndexes     = new int[employees.length];
-        int     numOfBdaysToday = 0;
-
-        for(int i = 0; i < employees.length; i++){
-            if(employees[i].wasBornToday()){
-                bDayIndexes[i] = i;
-                numOfBdaysToday++;
-            } else {
-                bDayIndexes[i] = -1;
-            }
-        }
-        
-        Employee[] out = new Employee[numOfBdaysToday];
-        
-        if(numOfBdaysToday > 0){
-            int outIndex = 0;
-            for(int i = 0; i < employees.length; i++){
-                if(bDayIndexes[i] == i){
-                    out[outIndex] = employees[i];
-                    outIndex++;
-                }
-            }
-        }
-
-        return out;
+        return Arrays.stream(employees)
+            .filter(employee -> employee.wasBornToday())
+            .toArray(Employee[]::new);
     }
 
     private static void printEmployees(Employee[] employees){
         for(Employee employee : employees){
-            System.out.println(employee);
+            System.out.println(employee.getFullName());
         }
     }
 }
